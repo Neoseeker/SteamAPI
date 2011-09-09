44e7a85af18598beb64e5f6084e9f137aed47010
Jamie Taniguchi <saveonfoods@gmail.com>
2011-09-09 07:58:05 -0700

    Classes for Steam

---
+++ SteamAPI.class.php
@@ -0,0 +1,91 @@
+<?php
+namespace steam;
+class SteamAPI {
+	var $information;
+	/** @var SteamAPIDriver $driver */
+	var $driver;
+
+	/**
+	 * @PdInject steamapidriver
+	 * @param SteamAPIDriver $driver
+	 */
+	public function __construct($driver) {
+		$this->driver = $driver;
+	}
+
+	public function load($steamid) {
+		$this->driver->set_user($steamid);
+		$this->information = $this->driver->get_xml_as_obj();
+		return $this;
+	}
+
+	public function get_games() {
+		$games = array();
+		/** @var \stdClass $xml_object */
+		$xml_object = $this->driver->get_games_xml_as_obj();
+		if (isset($xml_object->games->game)) {
+			$games = $this->create_games_array($xml_object->games->game);
+		}
+		return $games;
+	}
+
+	public function get_achievements_for_game($game_title = false, $stats_url = false) {
+		$achievements = array();
+		if ($game_title) {
+			$this->driver->set_game_stats_url($game_title);
+		} elseif ($stats_url) {
+			$this->driver->set_xml_location($stats_url.'?xml=1');
+		}
+		/** @var \stdClass $xml_object */
+		$xml_object = $this->driver->get_xml_as_obj();
+
+		if (isset($xml_object->achievements->achievement)) {
+			$achievements = $this->create_achievements_array($xml_object->achievements->achievement);
+		}
+		return $achievements;
+	}
+
+	/**
+	 * Please note that this can be a slow function as it is n+ where n is the number of
+	 * games the user has which have achievements. Use this at your own discretion.
+	 *
+	 * @see $this->get_games();
+	 * @see $this->get_achievements_for_game();
+	 * @return array	$games
+	 */
+	public function get_games_and_achievements() {
+		$games = $this->get_games();
+		if (count($games) > 0) {
+			foreach ($games as $game) {
+				if (isset($game->statsLink)) {
+					$game->achievements = $this->get_achievements_for_game(false, $game->statsLink);
+				}
+			}
+		}
+		return $games;
+	}
+
+	private function create_games_array($games) {
+		$games_array = array();
+		if (count($games) > 0) {
+			foreach ($games as $game) {
+				$games_array[$game->appID] = $game;
+			}
+		}
+		return $games;
+	}
+
+	private function create_achievements_array($achievements) {
+		$achievements_array = array();
+		if (count($achievements) > 0) {
+			foreach ($achievements as $achievement) {
+				$achievements_array['list'][$achievement->apiname] = $achievement;
+				if ($achievement->{'@attributes'}->closed == 1) {
+					$achievements_array['completed'][] = $achievement->apiname;
+				}
+			}
+		}
+		return $achievements_array;
+	}
+}
+?>
---
+++ SteamAPIDriver.class.php
@@ -0,0 +1,72 @@
+<?php
+namespace steam;
+class SteamAPIDriver {
+	private $_community_url;
+	private $_profile_path;
+	private $_api_format;
+	private $xml_location;
+
+	public function __construct() {
+		$this->_community_url = "http://steamcommunity.com/";
+		$this->_api_format = "?xml=1";
+	}
+
+	public function set_user($steamid) {
+		$this->_profile_path = (is_numeric($steamid)) ? "profiles/$steamid/" : "id/$steamid/";
+		$this->set_xml_location($this->_community_url.$this->_profile_path.$this->_api_format);
+		$this->set_games_xml_url();
+	}
+
+	/**
+	 * SimpleXMLElement is a resource, not an object so we'll use a
+	 * hack to make it into a useable object by encoding and decoding
+	 * it to and from JSON.
+	 *
+	 * @return	bool|\stdClass
+	 */
+	public function get_xml_as_obj() {
+		$xml_response = $this->get_xml();
+		$xml_object = $this->convert_to_object($xml_response);
+		return $xml_object;
+	}
+
+	public function get_xml($resource = null) {
+		try {
+			$xml_response = simplexml_load_file($this->get_xml_location(), null, LIBXML_NOCDATA);
+			$this->unset_location();
+			return $xml_response;
+		} catch (\Exception $e) {
+			return false;
+		}
+	}
+
+	public function get_games_xml_as_obj() {
+		$this->set_xml_location($this->get_games_xml_url());
+		return $this->get_xml_as_obj();
+	}
+
+	public function set_games_xml_url() {
+		$this->games_xml_url = $this->_community_url.$this->_profile_path.'games'.$this->_api_format;
+	}
+
+	public function get_games_xml_url() {
+		return $this->games_xml_url;
+	}
+
+	public function get_xml_location() {
+		return $this->xml_location;
+	}
+
+	public function set_xml_location($location) {
+		$this->xml_location = $location;
+	}
+
+	private function convert_to_object($simplexml_object) {
+		return json_decode(json_encode($simplexml_object));
+	}
+
+	private function unset_location() {
+		unset($this->xml_location);
+	}
+}
+?>
