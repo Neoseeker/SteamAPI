96fc50c280a8883aede388975996b3c0657ee579
Jamie Taniguchi <saveonfoods@gmail.com>
2011-11-07 11:12:58 +0100

    Added in ability to use an offline mode which uses the xml files in /tests/xml

--- SteamAPIDriver.class.php
+++ SteamAPIDriver.class.php
@@ -5,6 +5,8 @@ class SteamAPIDriver {
 	private $_profile_path;
 	private $_api_format;
 	private $xml_location;
+	private $in_offline_mode;
+	private $static_xml_path = "/tests/xml/";
 
 	private $users_xml_url;
 	private $games_xml_url;
@@ -19,6 +21,10 @@ class SteamAPIDriver {
 		$this->set_users_xml_url($steamid);
 		$this->set_xml_location($this->get_users_xml_url());
 		$this->set_games_xml_url();
+	}
+
+	public function set_offline_mode($offline_mode = false) {
+		$this->in_offline_mode = (is_bool($offline_mode)) ? $offline_mode : false;
 	}
 
 	/**
@@ -56,7 +62,7 @@ class SteamAPIDriver {
 
 	public function set_users_xml_url($steamid) {
 		$this->_profile_path = (is_numeric($steamid)) ? "profiles/$steamid/" : "id/$steamid/";
-		$this->users_xml_url = $this->_community_url.$this->_profile_path.$this->_api_format;
+		$this->users_xml_url = (!$this->in_offline_mode) ? $this->_community_url.$this->_profile_path.$this->_api_format : $this->static_xml_path.'tekmosis.xml';
 	}
 
 	public function get_users_xml_url() {
@@ -64,7 +70,7 @@ class SteamAPIDriver {
 	}
 
 	public function set_games_xml_url() {
-		$this->games_xml_url = $this->_community_url.$this->_profile_path.'games'.$this->_api_format;
+		$this->games_xml_url = (!$this->in_offline_mode) ? $this->_community_url.$this->_profile_path.'games'.$this->_api_format : $this->static_xml_path.'games.xml';
 	}
 
 	public function get_games_xml_url() {
@@ -72,7 +78,7 @@ class SteamAPIDriver {
 	}
 
 	public function set_stats_xml_url($game_title) {
-		$this->stats_xml_url = $this->_community_url.$this->_profile_path.'stats/'.$game_title.$this->_api_format;
+		$this->stats_xml_url = (!$this->in_offline_mode) ? $this->_community_url.$this->_profile_path.'stats/'.$game_title.$this->_api_format : $this->static_xml_path.'stats.xml';
 	}
 
 	public function get_stats_xml_url() {
@@ -85,6 +91,10 @@ class SteamAPIDriver {
 
 	public function set_xml_location($location) {
 		$this->xml_location = $location;
+	}
+
+	public function set_static_xml_path($static_xml_path) {
+		$this->static_xml_path = $static_xml_path;
 	}
 
 	private function convert_to_object($simplexml_object) {
